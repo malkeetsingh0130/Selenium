8858b1815ce59c40aecac82de51c23c73c8e6bee
Malkeet <55949501+malkeetsingh0130@users.noreply.github.com>
2025-06-04 22:31:42 +0530

    Delete Python directory

--- Python/elevatorfloor.py
+++
@@ -1,8 +0,0 @@
-inp=input("Which country are you in? ").lower()
-floor=int(input("Which floor are you on? "))
-print(type(inp))
-if inp!="us":
-    usfloor=floor+1
-    print("us floor",usfloor)
-else:
-    print("US floor",floor)
--- Python/type conversion.py
+++
@@ -1,3 +0,0 @@
-weight_pounds=input('Enter your weight: ')
-weight_kg= float(weight_pounds)/2.20462
-print(weight_kg)
